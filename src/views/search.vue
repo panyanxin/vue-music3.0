2661cd2bb0c7fd39b7fbb39c8e63fda1073e3f74
panyanxin <1205307815@qq.com>
2021-07-18 20:26:30 +0800

    search 界面开发 以及数据获取

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,13 +1,119 @@
 <template>
-  <div>recommend</div>
+  <div class="search">
+    <div class="search-input-wrapper">
+      <SearchInput v-model="query"></SearchInput>
+    </div>
+    <div class="search-content" v-show="!query">
+      <div class="hot-keys">
+        <h1 class="title">热门搜索</h1>
+        <ul>
+          <li 
+            v-for="item in hotKeys"
+            :key='item.id'
+            class="item"
+            @click='addQuery(item.key)'
+          >
+            <span>{{item.key}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="search-result" v-show="query">
+      <Suggest 
+        :query="query"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import SearchInput from '@/components/search/search-input'
+import Suggest from '@/components/search/suggest'
+import {ref, watch} from 'vue'
+import { getHotKeys } from '@/service/search'
 export default {
-
+  name: 'search',
+  setup() {
+    const query = ref('')
+    const hotKeys = ref([])
+    getHotKeys().then((result) => {
+      hotKeys.value = result.hotKeys
+    })
+    function addQuery(val) {
+      query.value = val
+    }
+    // watch(query, (val) => {
+      
+    // })
+    return {
+      query,
+      hotKeys,
+      addQuery
+    }
+  },
+  components: {
+    SearchInput,
+    Suggest
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .search {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    .search-input-wrapper {
+      margin: 20px;
+    }
+    .search-content {
+      flex: 1;
+      overflow: hidden;
+      .hot-keys {
+        margin: 0 20px 20px 20px;
+        .title {
+          margin-bottom: 20px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+        }
+        .item {
+          display: inline-block;
+          padding: 5px 10px;
+          margin: 0 20px 10px 0;
+          border-radius: 6px;
+          background: $color-highlight-background;
+          font-size: $font-size-medium;
+          color: $color-text-d;
+        }
+      }
+      .search-history {
+        position: relative;
+        margin: 0 20px;
+        .title {
+          display: flex;
+          align-items: center;
+          height: 40px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+          .text {
+            flex: 1;
+          }
+          .clear {
+            @include extend-click();
+            .icon-clear {
+              font-size: $font-size-medium;
+              color: $color-text-d;
+            }
+          }
+        }
+      }
+    }
+    .search-result {
+      flex: 1;
+      overflow: hidden;
+    }
+  }
 </style>
