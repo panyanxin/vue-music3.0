cd00a3f94aaf280afb81e84c0da412e1b564e3aa
panyanxin <1205307815@qq.com>
2021-07-24 23:41:11 +0800

    路由组件异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,24 @@ import {
   createRouter,
   createWebHashHistory
 } from 'vue-router'
-import Recommend from '@/views/recommend'
-import Singer from '@/views/singer'
-import SingerDetail from '@/views/singer-detail'
-import Search from '@/views/search'
-import TopList from '@/views/top-list'
-import TopDetail from '@/views/top-detail'
+// import Recommend from '@/views/recommend'
+// import Singer from '@/views/singer'
+// import SingerDetail from '@/views/singer-detail'
+// import Search from '@/views/search'
+// import TopList from '@/views/top-list'
+// import TopDetail from '@/views/top-detail'
+// import Album from '@/views/album'
+// import UserCenter from '@/views/user-center'
+
+const Recommend = () => import('@/views/recommend'/* webpackChunkName: "recommend" */)
+const Singer = () => import('@/views/singer'/* webpackChunkName: "singer" */)
+const TopList = () => import('@/views/top-list'/* webpackChunkName: "top-list" */)
+const Search = () => import('@/views/search'/* webpackChunkName: "search" */)
+const SingerDetail = () => import('@/views/singer-detail'/* webpackChunkName: "singer-detail" */)
 const Album = () => import('@/views/album'/* webpackChunkName: "album" */)
-import UserCenter from '@/views/user-center'
-// const UserCenter = () => import('@/views/user-center'/* webpackChunkName: "album" */)
+const TopDetail = () => import('@/views/top-detail'/* webpackChunkName: "top-detail" */)
+const UserCenter = () => import('@/views/user-center'/* webpackChunkName: "user-center" */)
+
 const routes = [{
     path: '/',
     redirect: '/recommend'
